a92f00f3b5d1a9d669be8fc51b5f3b4319ccf88a
Devanshi Mishra <69320890+devanshi-code@users.noreply.github.com>
2022-01-06 12:54:58 +0530

    Update RUNLENGTHENCODING.cpp

--- RUNLENGTHENCODING.cpp
+++ RUNLENGTHENCODING.cpp
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 string encode(string src){
   int i=0,j=0;
     string ans="";
@@ -11,18 +11,5 @@ string encode(string src){
         
     }
     return ans;
-=======
-string encode(string src){
-  int i=0,j=0;
-    string ans="";
-    while(j<src.size()){
-        while(src[i]==src[j]){
-            j++;
-        }
-        ans+=src[i] + to_string(j-i);
-        i=j;
-        
-    }
-    return ans;
->>>>>>> 839cf12cc64ea5ca6537d140390f2aaa45008b96
+
 }
