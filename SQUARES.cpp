ab2729f5080375b7ec2adbb47a9d1fe1af353536
Devanshi Mishra <69320890+devanshi-code@users.noreply.github.com>
2022-01-06 12:55:54 +0530

    Update SQUARES.cpp

--- SQUARES.cpp
+++ SQUARES.cpp
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 class Solution {
   public:
     long long squaresInChessBoard(long long N) {
@@ -7,26 +7,3 @@ class Solution {
     }
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=======
-class Solution {
-  public:
-    long long squaresInChessBoard(long long N) {
-        // code here
-        return (N * (N + 1) / 2) * (2 * N + 1) / 3;
-    }
-};
->>>>>>> 839cf12cc64ea5ca6537d140390f2aaa45008b96
