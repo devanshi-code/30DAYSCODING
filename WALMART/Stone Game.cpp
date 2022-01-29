24446955758e5d56667848a9a7befa541cc44e6e
devanshi-code <devu2732@gmail.com>
2022-01-29 21:16:57 +0530

    commit

---
+++ WALMART/Stone Game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int dp[501][501];
+    int help(int i,int j,vector<int>&p)
+    {
+        if(i>j) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        return dp[i][j]=max(p[i]-help(i+1,j,p),p[j]-help(i,j-1,p));
+    }    
+    bool stoneGame(vector<int>& p) {
+       int n=p.size(); 
+       memset(dp,-1,sizeof dp);
+       return help(0,n-1,p)>0;
+        
+    }
+};
