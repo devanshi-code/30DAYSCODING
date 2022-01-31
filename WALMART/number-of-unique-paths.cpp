975091fe298ece8799768a79d42d019ee25159fa
devanshi-code <devu2732@gmail.com>
2022-01-31 23:32:00 +0530

    commit

---
+++ WALMART/number-of-unique-paths.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+    public:
+    int NumberOfPath(int a, int b)
+    {
+        vector<vector<int>> M(a, vector<int>(b, 1));
+        
+        for(int r = 1; r < a; ++r) {
+            for(int c = 1; c < b; ++c) {
+                M[r][c] = M[r-1][c] + M[r][c-1];
+            }
+        }
+
+        return M.back().back();
+    }
+};
